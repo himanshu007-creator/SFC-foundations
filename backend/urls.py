973db41c2d58b74a97f0a864433fa3d1b364667a
Aditya <aditya201551@gmail.com>
2021-05-08 23:47:22 +0530

    initial setup for blog

--- backend/urls.py
+++ backend/urls.py
@@ -15,12 +15,24 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url
 from app import views
 urlpatterns = [
     #general
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
-    path('blog/', views.blog, name="blog"),
+    # path('blog/', views.blog, name="blog"),
+    url(r'^$',views.PostListView.as_view(),name='post_list'),
+    url(r'^about/$',views.AboutView.as_view(),name='about'),
+    url(r'^post/(?P<pk>\d+)$', views.PostDetailView.as_view(), name='post_detail'),
+    url(r'^post/new/$', views.CreatePostView.as_view(), name='post_new'),
+    url(r'^post/(?P<pk>\d+)/edit/$', views.PostUpdateView.as_view(), name='post_edit'),
+    url(r'^drafts/$', views.DraftListView.as_view(), name='post_draft_list'),
+    url(r'^post/(?P<pk>\d+)/remove/$', views.PostDeleteView.as_view(), name='post_remove'),
+    url(r'^post/(?P<pk>\d+)/publish/$', views.post_publish, name='post_publish'),
+    url(r'^post/(?P<pk>\d+)/comment/$', views.add_comment_to_post, name='add_comment_to_post'),
+    url(r'^comment/(?P<pk>\d+)/approve/$', views.comment_approve, name='comment_approve'),
+    url(r'^comment/(?P<pk>\d+)/remove/$', views.comment_remove, name='comment_remove'),
     path('contact/', views.contact, name="contact"),
     path('events/', views.events, name='event'),
     path('joinus/', views.join, name='join'),
