3f94f43c94095653d7c286676f5eb23a6cbcbcfe
Aditya <aditya201551@gmail.com>
2021-05-08 17:25:47 +0530

    switch

--- app/models.py
+++ app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+from django.utils import timezone
+from django.urls import reverse
 # Create your models here.
+ 
+class blog(models.Model):
+    admin=models.ForeignKey('auth.User')
+    author=models.CharField(max_length=100)
+    title=models.CharField(max_length=200)
+    text = models.CharField()
+    date=models.DateTimeField(default=timezone.now())
