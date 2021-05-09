c45bb9ab1cc83cc9ec8377c7a1967fbb905d222e
Aditya <aditya201551@gmail.com>
2021-05-09 19:26:45 +0530

    scratch

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from app.models import *
 # Register your models here.
+
+admin.site.register(BlogPost)
--- backend/settings.py
+++ backend/settings.py
@@ -16,7 +16,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATE_DIR=os.path.join(BASE_DIR,'app/template')
-
+MEDIA_ROOT =os.path.join(BASE_DIR,'app/media')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -127,3 +127,5 @@ STATIC_ROOT=os.path.join(BASE_DIR,'static')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL='/'
+
+MEDIA_URL = "media/"
