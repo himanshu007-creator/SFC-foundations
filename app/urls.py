e4e86bef81dc3e3f900e85347d7bde2311b160d7
naman153 <153namangupta@gmail.com>
2021-05-12 01:17:26 +0530

    Added Comment Feature in Backend

--- app/urls.py
+++ app/urls.py
@@ -5,5 +5,5 @@ from django.conf.urls.static import static
 from django.conf import settings
 urlpatterns = [
     path('',views.BlogListView.as_view(),name='blog'),
-    path('blog/<int:pk>',views.BlogDetailView.as_view(),name='BlogDetailView'),
+    path('blog/<int:pk>',views.BlogDetailView,name='BlogDetailView'),
 ]
